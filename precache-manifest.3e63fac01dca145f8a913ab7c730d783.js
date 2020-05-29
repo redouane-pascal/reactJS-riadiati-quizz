ddbcbfd1fc791ac2d73388a8e56af6c1ffaaea8b
redouane-pascal <hecto85@hotmail.com>
2020-05-28 23:47:41 -0400

    Updates

---
+++ precache-manifest.3e63fac01dca145f8a913ab7c730d783.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57628af28ea37069d2439e225a9243f",
+    "url": "/reactJS-riadiati-quizz/index.html"
+  },
+  {
+    "revision": "68efbd22009e2ddc7ee8",
+    "url": "/reactJS-riadiati-quizz/static/css/2.d07bc061.chunk.css"
+  },
+  {
+    "revision": "a2801e2ba976f5034a72",
+    "url": "/reactJS-riadiati-quizz/static/css/main.49a4f0d7.chunk.css"
+  },
+  {
+    "revision": "68efbd22009e2ddc7ee8",
+    "url": "/reactJS-riadiati-quizz/static/js/2.d45c57d5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/reactJS-riadiati-quizz/static/js/2.d45c57d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2801e2ba976f5034a72",
+    "url": "/reactJS-riadiati-quizz/static/js/main.a49f185b.chunk.js"
+  },
+  {
+    "revision": "93236581024b117ec474",
+    "url": "/reactJS-riadiati-quizz/static/js/runtime-main.57650cd3.js"
+  },
+  {
+    "revision": "bed041e1537bd394ce70c63d913f9b71",
+    "url": "/reactJS-riadiati-quizz/static/media/bg_riadiati.bed041e1.png"
+  },
+  {
+    "revision": "9007e38eea99148598c0ebb57536c00a",
+    "url": "/reactJS-riadiati-quizz/static/media/test.9007e38e.png"
+  }
+]);
